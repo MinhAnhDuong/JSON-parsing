3f46b97cc8b48a159f7a40eebf8b1c3e88ec948c
MinhAnhDuongova <minhanhduong97@gmail.com>
2022-11-04 18:48:33 +0100

    Adding exceptions

--- test.py
+++ test.py
@@ -5,35 +5,44 @@ with open("sample-data.json","r") as file:
 
     for virtual_machine_cfg in list_data:
         try:
-            #name
             print("------------------------------------")
+
             name = virtual_machine_cfg["name"]
             print(f"name: {name}")
+        except (KeyError, TypeError):
+            print("name: value not found")
 
-            #cpu
+        try:
             cpu = virtual_machine_cfg["state"]["cpu"]["usage"]
             print(f"CPU: {cpu}")
+        except (KeyError, TypeError):
+            print("CPU: value not found")
 
-            #memory_usage
+        try:
             memory_usage = virtual_machine_cfg["state"]["memory"]["usage"]
             print(f"memory usage: {memory_usage}")
+        except (KeyError, TypeError):
+            print("memory usage: value not found")
 
-            #date
+        try:
             date_of_create = virtual_machine_cfg["created_at"]
             print(f"date: {date_of_create}")
+        except (KeyError, TypeError):
+            print("date: value not found")
 
-            #status
+        try:
             status = virtual_machine_cfg["status"]
             print(f"status: {status}")
+        except (KeyError, TypeError):
+            print("status: value not found")
 
-            #adresses
+        try:
             networks = virtual_machine_cfg["state"]["network"]
-            
+
             for network in networks:
                 addresses = virtual_machine_cfg["state"]["network"][network]["addresses"]
 
                 for address in addresses:
                     print(f"address: {address['address']}")
-
-        except TypeError:
-            print("data not found")
+        except (KeyError, TypeError):
+            print("address: value not found")

9e5609696045a186f52f6166c3e59008a943a5c1
MinhAnhDuongova <minhanhduong97@gmail.com>
2022-11-04 21:58:17 +0100

    Edit instance variable and IP address filtering (append addresses to the list_of_address)

--- test.py
+++ test.py
@@ -5,14 +5,20 @@ class MyFilter:
     def __init__(self, path_to_file):
         self.path_to_file = path_to_file
         self.list_of_vms = list()
-        self.vm = {
-            "name": str(),
-            "memory_usage": int(),
-            "status": str(),
-            "date": str(),
-            "cpu": int(),
-            "address": list(),
-        }
+        self.name = str()
+        self.memory_usage = int()
+        self.status = str()
+        self.date = str()
+        self.cpu = int()
+        self.address = str()
+        self.vm = (
+            self.name,
+            self.memory_usage,
+            self.status,
+            self.date,
+            self.cpu,
+            self.address,
+        )
 
     def filter(self):
         with open(self.path_to_file, "r") as file:
@@ -21,45 +27,53 @@ class MyFilter:
             for virtual_machine_cfg in list_data:
                 try:
                     name = virtual_machine_cfg["name"]
-                    self.vm["name"] = name
+                    self.name = name
                 except (KeyError, TypeError):
-                    self.vm["name"] = "value not found"
+                    self.name = "value not found"
 
                 try:
                     cpu = virtual_machine_cfg["state"]["cpu"]["usage"]
-                    self.vm["cpu"] = cpu
+                    self.cpu = cpu
                 except (KeyError, TypeError):
-                    self.vm["cpu"] = "value not found"
+                    self.cpu = "value not found"
 
                 try:
                     memory_usage = virtual_machine_cfg["state"]["memory"]["usage"]
-                    self.vm["memory_usage"] = memory_usage
+                    self.memory_usage = memory_usage
                 except (KeyError, TypeError):
-                    self.vm["memory_usage"] = "value not found"
+                    self.memory_usage = "value not found"
 
                 try:
                     date_of_create = virtual_machine_cfg["created_at"]
-                    self.vm["date"] = date_of_create
+                    self.date = date_of_create
                 except (KeyError, TypeError):
-                    self.vm["date"] = "value not found"
+                    self.date = "value not found"
 
                 try:
                     status = virtual_machine_cfg["status"]
-                    self.vm["status"] = status
+                    self.status = status
                 except (KeyError, TypeError):
-                    self.vm["status"] = "value not found"
+                    self.status = "value not found"
 
                 try:
                     networks = virtual_machine_cfg["state"]["network"]
+                    list_of_address = []
 
                     for network in networks:
                         addresses = virtual_machine_cfg["state"]["network"][network]["addresses"]
 
                         for address in addresses:
-                            self.vm["address"] = address["address"]
+                            list_of_address.append(address["address"])
+                            self.address = list_of_address
                 except (KeyError, TypeError):
-                    self.vm["address"] = "value not found"  
+                    self.address = "value not found"
+
+                self.vm = (self.name, self.memory_usage, self.status, self.date, self.cpu, self.address)   
+                self.list_of_vms.append(self.vm)
+                
+        return self.list_of_vms
 
 test_filter = MyFilter("sample-data.json")
 filtered = test_filter.filter()
-print(test_filter.vm)
+print(len(filtered))
+print(filtered)
